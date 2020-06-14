b18395402cbfdd8f686d75ebae1230a7d1dd310b
Maxim <s_maxim_v@bk.ru>
2020-06-14 22:33:22 +0300

    Add LocationsViewController

---
+++ Indoor Navigation/Indoor Navigation/Extensions/UIView+setupShadow.swift
@@ -0,0 +1,9 @@
+//
+//  UIView+setupShadow.swift
+//  Indoor Navigation
+//
+//  Created by Maxim Sidorov on 14.06.2020.
+//  Copyright © 2020 Maxim Sidorov. All rights reserved.
+//
+
+import Foundation
---
+++ Indoor Navigation/Indoor Navigation/Interface/Views/LocationCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationCell.swift
+//  Indoor Navigation
+//
+//  Created by Maxim Sidorov on 14.06.2020.
+//  Copyright © 2020 Maxim Sidorov. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
